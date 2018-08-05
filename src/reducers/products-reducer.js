8b3c5826d0f64c22fef257317bbbe35f6b510188
Sameer Sarmah <sameerparansarmah@gmail.com>
2018-08-05 10:20:26 +0530

    adding jest tests

--- src/reducers/products-reducer.js
+++ src/reducers/products-reducer.js
@@ -1,17 +1,16 @@
-import _ from 'lodash';
-import { FETCH_PRODUCTS,FETCH_COUNT } from '../actions/types';
+import { FETCH_COUNT, FETCH_PRODUCTS } from '../actions/types';
 
 export default function(state = {count:0,products:[]}, action) {
   switch (action.type) {
-    case FETCH_PRODUCTS:{
-      const products =action.payload
-      return {count:state.count,products:state.products.concat(products)};
-    }
-    case FETCH_COUNT:{
-      const count = action.payload;
-      return {products:state.products,"count":count};
-    }
-     default:
-     return state; 
+  case FETCH_PRODUCTS:{
+    const products =action.payload;
+    return {count:state.count,products:state.products.concat(products)};
+  }
+  case FETCH_COUNT:{
+    const count = action.payload;
+    return {products:state.products,'count':count};
+  }
+  default:
+    return state; 
   }
 }
